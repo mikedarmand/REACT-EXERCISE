a4782840cd82e625022d6034f3e134eb0726f64c
mikedarmand <mikedarmand@users.noreply.github.com>
2024-05-31 12:46:23 +0200

    events

---
+++ Desktop/HANDLEEVENTS/handleEvents/src/App.jsx
@@ -0,0 +1,16 @@
+import MouseClicker from "./MouseClicker"
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>my handleEvents project</h1>
+     <MouseClicker/>
+    </>
+  )
+}
+
+export default App
